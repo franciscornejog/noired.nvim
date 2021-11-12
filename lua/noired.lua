323a769e481d22dea2dad0ccf4a0669fc7d74bdd
Francisco Cornejo-Garcia <franciscornejog@gmail.com>
2021-11-11 21:45:13 -0500

    Update colorscheme

--- lua/noired.lua
+++ lua/noired.lua
@@ -1,19 +1,14 @@
 -- noired.lua
 -- Author: Francisco Cornejo-Garcia (franciscornejog)
 
+local highlight = vim.api.nvim_set_hl
+local set_namespace = vim.api.nvim__set_hl_ns or vim.api.nvim_set_hl_ns
+local namespace = vim.api.nvim_create_namespace('noired')
+
 local function load_highlight(statement)
 	for name, setting in pairs(statement) do
-		local fg = setting.fg and ' guifg=' .. setting.fg or ' guifg=NONE'
-		local bg = setting.bg and ' guibg=' .. setting.bg or ' guibg=NONE'
-		local gui = setting.gui and ' gui=' .. setting.gui or ' gui=NONE'
-		vim.cmd('highlight ' .. name .. gui .. fg .. bg)
+        highlight(namespace, name, setting)
 	end
-end
-
-local function link_highlight(statement)
-    for name, setting in pairs(statement) do
-        vim.cmd('highlight! link ' .. name .. ' ' .. setting)
-    end
 end
 
 local color = {
@@ -30,92 +25,106 @@ local color = {
 
 local statements = {
 	Normal = { fg = color.gray, bg = color.black },
-	Keyword = { fg = color.white, bg = color.black },
-	Constant = { fg = color.gray, bg = color.black },
-	String = { fg = color.red, bg = color.black },
-	Comment = { gui = 'italic', fg = color.darkgray, bg = color.black },
-	Number = { fg = color.red, bg = color.black },
-	Error = { fg = color.white, bg = color.red },
-	ErrorMsg = { fg = color.white, bg = color.red },
-	Search = { fg = color.statusgray, bg = color.lightgray },
-	IncSearch = { gui = 'reverse', fg = color.white, bg = color.lightgray },
-	DiffChange = { fg = color.red, bg = color.white },
-	DiffText = { gui = 'bold', fg = color.gray, bg = color.red },
-	SignColumn = { fg = color.red, bg = color.black },
-	SpellBad = { gui = 'undercurl', fg = color.white, bg = color.red },
-	SpellCap = { fg = color.white, bg = color.red },
-	SpellRare = { fg = color.red, bg = color.black },
-	WildMenu = { fg = color.darkgray, bg = color.white },
-	Pmenu = { fg = color.white, bg = color.darkgray },
-	PmenuThumb = { fg = color.black, bg = color.darkgray },
-	SpecialKey = { fg = color.black, bg = color.white },
-	MatchParen = { fg = color.black, bg = color.darkgray },
-	ColorColumn = { bg = color.blackbg },
-	CursorColumn = { bg = color.blackbg },
-	CursorLine = { bg = color.blackbg },
-	StatusLine = { gui = 'bold', fg = color.statusgray, bg = color.black },
-	StatusLineNC = { fg = color.darkgray, bg = color.black },
-	StatusLineTerm = { gui = 'bold', fg = color.statusgray, bg = color.black },
-	StatusLineTermNC = { fg = color.darkgray, bg = color.black },
-	Visual = { gui = 'reverse', fg = color.gray, bg = color.black },
-	TermCursor = { gui = 'reverse'},
-}
 
-local link_statements = {
-	Boolean = 'Normal',
-	Delimiter = 'Normal',
-	Identifier = 'Normal',
-	Title = 'Normal',
-	Debug = 'Normal',
-	Exception = 'Normal',
-	FoldColumn = 'Normal',
-	Macro = 'Normal',
-	ModeMsg = 'Normal',
-	MoreMsg = 'Normal',
-	Question = 'Normal',
-	Conditional = 'Keyword',
-	Statement = 'Keyword',
-	Operator = 'Keyword',
-	Structure = 'Keyword',
-	Function = 'Keyword',
-	Include = 'Keyword',
-	Type = 'Keyword',
-	Typedef = 'Keyword',
-	Todo = 'Keyword',
-	Label = 'Keyword',
-	Define = 'Keyword',
-	DiffAdd = 'Keyword',
-	diffAdded = 'Keyword',
-	diffCommon = 'Keyword',
-	Directory = 'Keyword',
-	PreCondit = 'Keyword',
-	PreProc = 'Keyword',
-	Repeat = 'Keyword',
-	Special = 'Keyword',
-	SpecialChar = 'Keyword',
-	StorageClass = 'Keyword',
-	SpecialComment = 'String',
-	CursorLineNr = 'String',
-	Character = 'Number',
-	Float = 'Number',
-	Tag = 'Number',
-	Folded = 'Number',
-	WarningMsg = 'Number',
-	iCursor = 'SpecialKey',
-	SpellLocal = 'SpellCap',
-	LineNr = 'Comment',
-	NonText = 'Comment',
-	DiffDelete = 'Comment',
-	diffRemoved = 'Comment',
-	PmenuSbar = 'Visual',
-	PmenuSel = 'Visual',
-	VisualNOS = 'Visual',
-	VertSplit = 'Visual',
-	VertSplit = 'StatusLineNC',
-	Cursor = 'StatusLine',
-	Underlined = 'SpellRare',
-	rstEmphasis = 'SpellRare',
-	diffChanged = 'DiffChange',
+	Constant = { fg = color.red },
+	String = { fg = color.red },
+	Character = { fg = color.red },
+	Number = { fg = color.red },
+	Boolean = { fg = color.red },
+	Float = { fg = color.red },
+
+	Identifier = { fg = color.white },
+	Function = { fg = color.darkgray },
+
+	Statement = { fg = color.darkgray },
+	Conditional = { fg = color.darkgray },
+	Repeat = { fg = color.darkgray },
+	Label = { fg = color.darkgray },
+	Operator = { fg = color.darkgray },
+	Keyword = { fg = color.darkgray },
+	Exception = { fg = color.darkgray },
+
+	PreProc = { fg = color.darkgray },
+	Include = { fg = color.darkgray },
+	Define = { fg = color.darkgray },
+	Macro = { fg = color.darkgray },
+	PreCondit = { fg = color.darkgray },
+
+	Type = { fg = color.darkgray },
+	StorageClass = { fg = color.darkgray },
+	Structure = { fg = color.darkgray },
+	Typedef = { fg = color.darkgray },
+
+	Special = { fg = color.darkgray },
+	SpecialChar = { fg = color.red },
+	Tag = { fg = color.red },
+	Delimiter = { fg = color.gray },
+	Comment = { fg = color.darkgray },
+	SpecialComment = { fg = color.white },
+	Debug = { fg = color.white },
+
+	Underlined = { fg = color.red },
+	Error = { fg = color.red },
+	ErrorMsg = { fg = color.red },
+	Todo = { fg = color.red },
+	NonText = { fg = color.darkgray },
+
+	Cursor = { fg = color.darkgray },
+	LineNr = { fg = color.darkgray },
+	CursorLineNr = { fg = color.red },
+	CursorLine = { bg = color.blackbg },
+	CursorColumn = { bg = color.blackbg },
+	ColorColumn = { bg = color.blackbg },
+
+	Visual = { bg = color.blackbg },
+    VisualNOS = { bg = color.blackbg },
+
+	DiffAdd = { fg = color.white },
+	DiffChange = { fg = color.red, bg = color.white },
+	DiffDelete = { fg = color.darkgray },
+	DiffText = { fg = color.gray, bg = color.red },
+	diffAdded = { fg = color.white, bg = color.black },
+	diffChanged = { fg = color.red, bg = color.white },
+	diffCommon = { fg = color.white, bg = color.black },
+	diffRemoved = { fg = color.darkgray, bg = color.black},
+
+	Directory = { fg = color.white },
+
+	FoldColumn = { fg = color.gray, bg = color.black },
+	Folded = { fg = color.red, bg = color.black },
+
+	Search = { fg = color.statusgray, bg = color.lightgray },
+	IncSearch = { fg = color.white, bg = color.red },
+
+	MatchParen = { fg = color.red },
+
+	ModeMsg = { fg = color.gray },
+	MoreMsg = { fg = color.gray },
+	WarningMsg = { fg = color.red, bg = color.black },
+
+	Pmenu = { fg = color.gray },
+	PmenuSel = { fg = color.red },
+	WildMenu = { fg = color.red },
+
+	Question = { fg = color.gray },
+
+	SignColumn = { fg = color.red },
+
+	SpecialKey = { fg = color.black, bg = color.white },
+
+	SpellBad = { fg = color.white, bg = color.red },
+	SpellCap = { fg = color.white, bg = color.red },
+	SpellLocal = { fg = color. white, bg = color.red },
+	SpellRare = { fg = color.red, bg = color.black },
+
+	StatusLine = { fg = color.statusgray, bg = color.black },
+	StatusLineNC = { fg = color.darkgray, bg = color.black },
+	StatusLineTerm = { fg = color.statusgray, bg = color.black },
+	StatusLineTermNC = { fg = color.darkgray, bg = color.black },
+
+	Title = { fg = color.gray, bg = color.black },
+
+	VertSplit = { fg = color.gray },
 }
 
 local noired = {}
@@ -125,11 +134,11 @@ noired.setup = function()
     vim.o.background = 'dark'
     vim.o.termguicolors = true
     vim.g.colors_name = 'noired'
-    if vim.fn.exists("syntax_on") then
+    if vim.fn.exists('syntax_on') then
         vim.cmd('syntax reset')
     end
     load_highlight(statements)
-    link_highlight(link_statements)
+    set_namespace(namespace)
 end 
 
 return noired
